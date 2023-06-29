db20ba1a5108c2f4ea22a0dcbc1ca4e969cd9cd6
WentsingNee <1205422966@qq.com>
2023-06-29 17:55:19 +0800

    增加项目配置文件

--- configure.sh
+++
@@ -1,5 +0,0 @@
-
- 
-cmake ../ -DCMAKE_BUILD_TYPE=Release \
-	-DCMAKE_PREFIX_PATH="C:/Program Files/opencv/build/x64/vc16/lib;C:/Program Files/Azure Kinect SDK v1.4.1/sdk/windows-desktop/amd64/release/lib/" \
-	-DDKSAVE_K4A_INCLUDE_PATH="C:\Program Files\Azure Kinect SDK v1.4.1\sdk\include"
