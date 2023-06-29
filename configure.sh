6706bb6a0903a828a8b113c45b02415b16aea091
WentsingNee <1205422966@qq.com>
2023-06-29 17:55:19 +0800

    add new branch xueting

---
+++ configure.sh
@@ -0,0 +1,5 @@
+
+ 
+cmake ../ -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_PREFIX_PATH="D:/DK_save_files/opencv/build/x64/vc16/lib/;C:/Program Files/Azure Kinect SDK v1.4.1/sdk/windows-desktop/amd64/release/lib/;D:/DK_save_files/PCL/" \
+	-DDKSAVE_K4A_INCLUDE_PATH="C:/Program Files/Azure Kinect SDK v1.4.1/sdk/include"
