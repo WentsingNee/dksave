b597ebfe2c5a6b4919e6d22aa48927d1eac8bf1d
WentsingNee <8090395+wentsingnee@user.noreply.gitee.com>
2024-09-12 21:57:12 +0800

    cmake pcl component

--- cmake/third_party/pcl.cmake
+++ cmake/third_party/pcl.cmake
@@ -1,7 +1,7 @@
 
 if (DKSAVE_ENABLE_PCL)
     message(STATUS "Finding PCL")
-    find_package(PCL REQUIRED)
+    find_package(PCL REQUIRED COMPONENTS common io)
     message(STATUS "Finding PCL -- Found")
 
     target_link_libraries(dksave PRIVATE ${PCL_LIBRARIES})
